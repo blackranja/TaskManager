5fff9b92e9d5d83845e250fc4cba59a4a381450e
Josphat karanja <josphatranja243@gmail.com>
2023-01-19 12:39:39 +0300

    Mongoose Query

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,14 @@
 const Task = require('../models/Task');
 
-const getAllTask = (req, res) => {
-    res.send("Get All Tasks");
+const getAllTask = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
+
 }
 const createTask = async (req, res) => {
     try {
