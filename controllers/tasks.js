b7ac5cb2d1f006301a1366fdb03104418fd0fbc0
Josphat karanja <josphatranja243@gmail.com>
2023-01-19 14:02:11 +0300

    Mongoose MiddleWare

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -22,16 +22,39 @@ const createTask = async (req, res) => {
 
 
 }
-const getTask = (req, res) => {
-    res.send('Get Single Task');
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOne({ _id: taskID });
+
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id:${taskID}` })
+        }
+        res.status(200).json({ task });
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
+
 
 }
 const updateTask = (req, res) => {
     res.send("Update A Task");
 
 }
-const deleteTask = (req, res) => {
-    res.send("Delete A Task");
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID });
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+
+        }
+        res.status(200).json({ task });
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 }
 
 module.exports = {
