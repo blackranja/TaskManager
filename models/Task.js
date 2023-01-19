188e72ab707ad7af31d5c55f1a91d03d96ef0564
Josphat karanja <josphatranja243@gmail.com>
2023-01-19 12:11:40 +0300

    Mongoose Schemas

--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String, completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'Must provide Name'],
+        trim: true,
+        maxlength: [20, 'Name cannot be more than 20 Characters']
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports = mongoose.model('Task', TaskSchema);
